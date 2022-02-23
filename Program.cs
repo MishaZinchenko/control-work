a8b2448d5ac22aa36b7f27aab2b5b470cb84b240
MishaZinchenko <96254509+MishaZinchenko@users.noreply.github.com>
2022-02-23 17:16:46 +0300

    Добавили решение задачи

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿string[] a = {"2", "hello", "wfd", "(+_+)", "(:':", "6785", ":)"};
+
+string[] d = new string[a.Length];
+
+for (int i = 0; i < a.Length; i++)
+{
+  if (a[i].Length <= "000".Length)
+{
+    d[i] = a[i];
+}
+}
+
+for (int j = 0; j < d.Length; j++)
+{
+    Console.Write(d[j] + "  ");
+}
